a080ef024eb74e218b4b791583eb8ef1478188b2
User <JEJ>
2021-02-16 00:08:05 +0900

    a

--- src/sever.js
+++ src/sever.js
@@ -8,7 +8,7 @@ app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "/assets")));
 
 app.get("/", (rep, res) => {
-  res.render("index");
+  res.render("main");
 });
 
 app.listen(PORT, () => {
