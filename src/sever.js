c43784accbc7c10271a0e47e0605c043b8234197
User <JEJ>
2021-02-16 01:42:22 +0900

    footer

--- src/sever.js
+++ src/sever.js
@@ -8,7 +8,7 @@ app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "/assets")));
 
 app.get("/", (rep, res) => {
-  res.render("main");
+  res.render("layouts/main");
 });
 
 app.listen(PORT, () => {
